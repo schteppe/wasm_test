8d6f02c0fbe8e29331d8da38a2b006c2b62cd18e
Stefan Hedman <stefan.hedman@starstable.com>
2018-08-24 10:39:08 +0200

    add threading example

--- src/main.cpp
+++ src/main.cpp
@@ -182,3 +182,35 @@ int main ( int argc, char *argv[] )
 
    esMainLoop ( &esContext );
 }
+
+/* 
+// Threading support
+#define NUM_THREADS     5
+#include <mutex>
+ void *PrintHello(void *threadid)
+ {
+    long tid;
+    tid = (long)threadid;
+    printf("Hello World! It's me, thread #%ld!\n", tid);
+    pthread_exit(NULL);
+ }
+
+ int main (int argc, char *argv[])
+ {
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+    std::mutex m;
+    m.lock();
+    m.unlock();
+    for(t=0; t<NUM_THREADS; t++){
+       printf("In main: creating thread %ld\n", t);
+       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+       if (rc){
+          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          exit(-1);
+       }
+    }
+
+    //pthread_exit(NULL);
+ } */
